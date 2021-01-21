0375377569255afb620ec9381dbf41acfd218977
vedikabinani10 <69752144+vedikabinani10@users.noreply.github.com>
2021-01-21 16:28:32 +0530

    Add files via upload

---
+++ js/food.js
@@ -0,0 +1,56 @@
+class Food{
+    constructor(){
+        this.foodStock = 15;
+        this.lastFed;
+        this.image = loadImage("images/Milk.png");
+    }
+
+    bedroom(){
+        background(bedroomIMG,550,500);
+    }
+
+    garden(){
+        background(gardenIMG,550,500);
+    }
+
+    washroom(){
+        background(washroomIMG,550,500);
+    }
+
+    updateFoodStock(foodStock){
+        this.foodStock = foodStock;
+    }
+
+    getFedTime(lastFed){
+        this.lastFed = lastFed;
+    }
+
+    deductFood(){
+        if(this.foodStock >0){
+            this.foodStock = this.foodStock - 1
+        }
+        return this.foodStock;
+    }
+
+    getFoodStock(){
+        return this.foodStock;
+    }
+
+    display(){
+        var x = 80, y = 100;
+
+        imageMode(CENTER);
+        image(this.image,720,220,70,70);
+
+        if(this.foodStock != 0){
+            for(var i =0; i<this.foodStock; i++){
+                if(i%10==0){
+                    x=80;
+                    y=y+50;
+                }
+                image(this.image,x,y,50,50);
+                x=x+30;
+            }
+        }
+    }
+}
